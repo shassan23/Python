7c299c67694639e9bb03cd71b096017ebd8b5dff
shassan23 <s.hassan7311@gmail.com>
2024-11-26 14:00:30 +0000

    finished #9

---
+++ #exercise 9 - Dictionaries.py
@@ -0,0 +1,25 @@
+#exercise 9 - Dictionaries 
+
+library = {
+    "Non-Fiction" : ["Fantasy: Harry Potter", 
+                     "Mystery: The Couple Next Door" , 
+                     "History: The Roman Empire"]
+}
+print(library)
+
+#exercise 10 - Functions
+number = 0
+def number_checker (number):
+    if number %2 == 0:
+        print("number is even")
+    else:
+        print("number is odd")
+number_checker (12)
+
+number = 3
+def number_checker (number):
+    if number %2 == 0:
+        print("number is even")
+    else:
+        print("number is odd")
+number_checker (number)
